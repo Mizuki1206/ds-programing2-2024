196ee5f853d004508a99464af6babe03680aac64
Mizuki1206 <s2422028@stu.musashino-u.ac.jp>
2024-11-27 12:17:51 +0900

    地域リストの表示

--- practice/jma/main.py
+++ practice/jma/main.py
@@ -13,5 +13,27 @@ def get_weather_data(area_code):
     url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area_code}.json"
     return requests.get(url).json()
 
+def main(page: ft.Page):
+    page.title = "天気予報アプリ"
+
+    area_dara = get_area_data()
+    
+
+    rail = ft.NavigationRail(
+        selected_index = 0,
+        label_type = ft.NavigationRailLabelType.All,
+        extended = True,
+        destinations = [
+            ft.NavigationRailDestination(
+                icon = ft.icons.FAVORITE_BORDER,
+                selected_icon = ft.icons.FAVORITE,
+                label = "地域を選択"
+            ),
+        ]
+    )
+
+    area_list = 
+
+
 
 ft.app(main)
