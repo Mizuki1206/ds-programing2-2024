b7c833026d72763ac88e9644671dd143382bd407
Mizuki1206 <s2422028@stu.musashino-u.ac.jp>
2024-11-27 10:12:34 +0900

    エリアと天気の情報を実装する。

--- practice/jma/main.py
+++ practice/jma/main.py
@@ -1,8 +1,17 @@
 import flet as ft
+import requests
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "天気予報アプリ"
+
+def get_area_data():
+    url = "https://www.jma.go.jp/bosai/common/const/area.json"
+    return requests.get(url).json()
+
+def get_weather_data(area_code):
+    url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area_code}.json"
+    return requests.get(url).json()
 
 
 ft.app(main)
